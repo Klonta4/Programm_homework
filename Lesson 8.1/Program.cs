3df18d17ce09843d4a87eac61ea6cd259fde8a80
Alexandr Voronin <sasha-voronin2001@mail.ru>
2023-01-30 22:08:29 +0300

    Task finished

--- Lesson 8.1/Program.cs
+++ Lesson 8.1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int m;
+int n;
+
+Console.WriteLine("Введите количество столбцов.");
+m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк.");
+n = int.Parse(Console.ReadLine());
+Random rnd = new Random();
+int[,] Arr = new int[n, m];
+
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < m; j++)
+    {
+        Arr[i,j] =(int)rnd.Next(0, 10);
+        Console.Write(Arr[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int h = 0; h < m; h++)
+		for (int j = 0; j < m - 1; j++)
+        {
+			if (Arr[i, j] < Arr[i, j + 1])
+			{
+	    		int t = Arr[i, j];
+				Arr[i, j] = Arr[i, j + 1];
+				Arr[i, j + 1] = t;
+			}
+        }
+}
+Console.WriteLine();
+Console.WriteLine();
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < m; j++)
+    {
+        Console.Write(Arr[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
