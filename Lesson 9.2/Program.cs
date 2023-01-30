a0a80a08f08f85078e6f13f1127c027229738bc3
Alexandr Voronin <sasha-voronin2001@mail.ru>
2023-01-30 23:53:45 +0300

    Second task already

---
+++ Lesson 9.2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите Первое число (большое)");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Второе число (Меньше прошлого)");
+int M = int.Parse(Console.ReadLine());
+
+void RecFunc(int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    sum = sum + (M++);
+    RecFunc(M, N, sum);
+}
+
+Console.Write("Сумма чисел от " + M + " до " + N + " равна: ");
+RecFunc(M, N, 0);
