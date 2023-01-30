eb34070f89ecd8c51b1f6e24fef86b7ee825b8a1
Alexandr Voronin <sasha-voronin2001@mail.ru>
2023-01-30 23:19:57 +0300

    First task

---
+++ Lesson 9.1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("");
+int N = int.Parse(Console.ReadLine());
+Console.Write("все натуральные числа от " + N + " до 1:");
+RecFunc(N);
+
+int RecFunc(int N)
+{
+    if (N == 0) return 1;
+    Console.Write(N + ", ");
+    return RecFunc(N - 1);
+}
+
