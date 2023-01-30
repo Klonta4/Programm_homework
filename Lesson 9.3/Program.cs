9d5e50d0ee69dfec373944ce633ab6227b42dc1d
Alexandr Voronin <sasha-voronin2001@mail.ru>
2023-01-31 00:09:02 +0300

    Ackerman function is wonder

---
+++ Lesson 9.3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число M");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"функция Акераманна для чисел A({M},{N}) : " + AccermanFunc(M, N));
+
+int AccermanFunc(int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M != 0 && N == 0) return AccermanFunc(M - 1, 1);
+    if (M > 0 && N > 0) return AccermanFunc(M - 1, AccermanFunc(M, N- 1));
+    return AccermanFunc(M,N);
+}
